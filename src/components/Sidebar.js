9964380b183500b0b13ad100ad06c729ec447ad5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:02:09 +0530

    Mapping sidebarItem Array sidebarItem.map((item, index) => { ... }): Iterates over the sidebarItem array. JSX Return: Returns a <div> element for each item in sidebarItem. Structure: <div key={index} className='flex my-3 ml-2'>: Flex container for each sidebar item with margin and padding. {item.icons}: Renders the icon JSX element from react-icons library. <p className={ml-5 ${open ? "" : 'hidden'}}>{item.title}</p>: Renders the title of the sidebar item. When open is true, the title is visible (ml-5 adds left margin). When open is false, the title is hidden (hidden class hides the element).

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -109,7 +109,14 @@ const Sidebar = () => {
     return (
         <div className={`relative left-0 ${open? "w-[20%]" : "w-[6%]"} p-5 h-[calc(100vh-4.625rem)] bg-white overflow-y-scroll overflow-x-hidden`}>
             {
-                
+                sidebarItem.map((item, index) => {
+                    return (
+                        <div key={index} className='flex my-3 ml-2'>
+                            {item.icons}
+                            <p className={`ml-5 ${open ? "": 'hidden'}`}>{item.title}</p>
+                        </div>
+                    )
+                })
             }
         </div>
     )
