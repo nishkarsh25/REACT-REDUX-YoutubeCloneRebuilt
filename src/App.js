3b9c977b161260d356432400603edb7d2a9e531f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:27:11 +0530

    Router Configuration The routing is configured using createBrowserRouter and RouterProvider from react-router-dom. The appRouter defines the structure of the routes in the application:
    Root Path (/):
    Element: <Body />
    Children:
    Path /:
    Element: <Feed />
    Path /watch:
    Element: <Watch />

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Watch from "./components/Watch";
 import Feed from "./components/Feed";
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <Feed />
+      },
+      {
 
+        path: "/watch",
+        element: <Watch />
+      }
+    ]
+  }
+])
 
 
 
