884324ba04c24b69a24da70a8d0bb4ab2882256a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:01:02 +0530

    sidebarItem Array Definition: sidebarItem is an array of objects, where each object represents an item in the sidebar menu. Structure: Each object has two properties: icons: JSX element representing an icon from the react-icons library, sized at 24px. title: String representing the title or label for the sidebar item.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,103 @@ import { SiYoutubeshorts } from "react-icons/si";
 import { MdOutlineSubscriptions } from "react-icons/md";
 import {useSelector} from "react-redux";
 
-
+const sidebarItem = [
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+    {
+        icons: <CiHome size="24px" />,
+        title: "Home"
+    },
+    {
+        icons: <SiYoutubeshorts size="24px" />,
+        title: "Short"
+    },
+    {
+        icons: <MdOutlineSubscriptions size="24px" />,
+        title: "Subscription"
+    },
+]
 
 
