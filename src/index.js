68e27a8fe661c60361fba1c444c47fe0e76e98ea
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 15:46:47 +0530

    JSX Structure: <React.StrictMode>: Wraps the entire application in React.StrictMode. This component is used for highlighting potential problems in the application during development (such as deprecated lifecycle methods and legacy string ref usage). <Provider store={store}>: Wraps the <App /> component with the Redux Provider component, passing the Redux store as a prop. This provides the Redux store to all components in the application, allowing them to access the Redux state and dispatch actions. <App />: The main component of the application, imported from './App'. This component is rendered within the <Provider> component, allowing it and its child components to access Redux store state via React Redux's connect function or hooks like useSelector and useDispatch.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import store from './utils/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
