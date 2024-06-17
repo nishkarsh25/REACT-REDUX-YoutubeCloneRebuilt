71035c6202d54c241d47921322c245a5260e8637
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:14:48 +0530

    Functions fetchingYoutubeVideo: An asynchronous function that fetches videos from the YouTube API.
    Uses axios to send a GET request to YOUTUBE_VIDEO_API.
    Dispatches the setHomeVideo action with the fetched data to update the video state in the Redux store.
    Includes error handling to log any errors that occur during the request.

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -12,7 +12,16 @@ const VideoContainer = () => {
     const { video, category } = useSelector((store) => store.app);
     console.log(category);
     const dispatch = useDispatch();
-    
+    // jab bhi api call/network call kr rhe ho toh apko hmesha useEffect use krna hai
+    const fetchingYoutubeVideo = async () => {
+        try {
+            const res = await axios.get(`${YOUTUBE_VIDEO_API}`);
+            dispatch(setHomeVideo(res?.data?.items))
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
     
 
     
