37010b26140bd678355df12752bdfc3b93bd2725
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:08:52 +0530

    Data buttonList: An array of strings representing the names of the categories or tags that can be selected. These categories will be rendered as buttons.

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import {useDispatch} from "react-redux";
 import { setCategory } from '../utils/appSlice';
 
+const buttonList = ["All", "Javascript", "Java", "Live", "Music", "Songs", "Vlogs", "Trending", "Programming", "News", "Technology", "Cricket", "Comedy", "Thriller", "New to you", "Computer Programming", "Netlify", "Coding"]
 
 
 
