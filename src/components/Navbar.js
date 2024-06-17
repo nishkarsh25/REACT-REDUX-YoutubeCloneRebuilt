bba9f97873142f64b7dfd8925a581b31568ff177
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:03:59 +0530

    Imports React: Imported from 'react'. Necessary for creating React components and utilizing JSX syntax. Icons from react-icons libraries: GiHamburgerMenu from 'react-icons/gi': Represents a hamburger menu icon. IoIosNotificationsOutline from 'react-icons/io': Represents a notifications icon. CiVideoOn from 'react-icons/ci': Represents a video camera icon. Avatar: Imported from 'react-avatar'. This component is used to display avatars, likely for user profiles. Hooks from 'react-redux': useDispatch: Hook used to dispatch actions to the Redux store. useSelector: Hook used to extract data from the Redux store state. useState: Imported from 'react'. This hook is used to manage local component state. axios: Imported from 'axios'. This library is used for making HTTP requests. SEARCH_SUGGESTIONS_API: Imported from '../constant/youtube'. This likely represents a constant URL endpoint for fetching search suggestions.

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { CiVideoOn } from "react-icons/ci";
+import Avatar from "react-avatar";
+import { CiSearch } from "react-icons/ci";
+import { useDispatch, useSelector } from "react-redux"
+import { toggleSidebar, setCategory, setSearchSuggestion } from "../utils/appSlice";
+import { useState } from "react";
+import { SEARCH_SUGGESTIONS_API } from "../constant/youtube";
+import axios from "axios";
+
+
+
