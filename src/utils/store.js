03901a27e83b2f272dd15ac6d512b46149acd7d6
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 15:45:17 +0530

    configureStore Function configureStore: The configureStore function is called with an object as an argument. reducer Object: This object specifies the reducers that will be combined into the root reducer for the Redux store. app: The key "app" is associated with appReducer, which presumably is the reducer function imported from "./appSlice". This connects the slice of state managed by appReducer (from the "appSlice") to the app key in the Redux store. chat: The key "chat" is associated with chatReducer, which similarly connects the slice of state managed by chatReducer (from the "chatSlice") to the chat key in the Redux store.

--- src/utils/store.js
+++ src/utils/store.js
@@ -2,5 +2,10 @@ import {configureStore} from "@reduxjs/toolkit";
 import appReducer from "./appSlice";
 import chatReducer from "./chatSlice";
 
-
+const store = configureStore({
+    reducer:{
+        app:appReducer,
+        chat:chatReducer
+    }
+})
 
