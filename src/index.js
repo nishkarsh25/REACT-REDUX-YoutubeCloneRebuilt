11539f5811daa01b4c55391f72cf9a3def44b8a3
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 15:46:21 +0530

    root.render Root Render Method: root.render(...) renders the React application into the DOM using the root instance created above.

--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ import { Provider } from "react-redux";
 import store from './utils/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+root.render(
+  <React.StrictMode>
+    
+  </React.StrictMode>
+);
