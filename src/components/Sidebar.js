c0a7deb44b79f03bdfebe12de05f44f82d185580
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:00:52 +0530

    Imports React: Imported from 'react'. Necessary for creating React components and utilizing JSX syntax. Icons from react-icons libraries: CiHome from 'react-icons/ci': Represents a home icon. SiYoutubeshorts from 'react-icons/si': Represents a YouTube shorts icon. MdOutlineSubscriptions from 'react-icons/md': Represents a subscriptions icon. useSelector: Imported from 'react-redux'. This hook is used to access and extract data from the Redux store.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { CiHome } from "react-icons/ci";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import {useSelector} from "react-redux";
+
+
+
+
