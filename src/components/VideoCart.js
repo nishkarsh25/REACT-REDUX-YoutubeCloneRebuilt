41b8d5949387d6e08418d19a7cd30f7c69102c02
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:17:25 +0530

    Functions getYoutubeChannelName: An asynchronous function to fetch the YouTube channel's avatar. Constructs the API URL using the channel ID from the item prop and the API_KEY. Uses axios to send a GET request to the YouTube API. Sets the ytIcon state with the URL of the channel's avatar fetched from the API response. Includes error handling to log any errors that occur during the request.

--- src/components/VideoCart.js
+++ src/components/VideoCart.js
@@ -6,7 +6,14 @@ import API_KEY from '../constant/youtube';
 
 const VideoCart = ({item}) => {
     const [ytIcon, setYtIcon] = useState("");
-    
+    const getYoutubeChannelName = async () =>{
+        try {
+            const res = await axios.get(`https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${item.snippet.channelId}&key=${API_KEY}`)
+            setYtIcon(res.data.items[0].snippet.thumbnails.high.url);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     
 
