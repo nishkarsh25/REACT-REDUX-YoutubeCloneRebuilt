5f8c887f3dbcdc0f911dd32fac1643197769b674
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 15:40:24 +0530

    appSlice Definition createSlice Function:
    createSlice is called with an object that defines a Redux slice.
    name: This specifies the name of the slice, which is "app" in this case.
    initialState: This defines the initial state of the slice. It includes several fields:
    open: Initialized to true, likely used to control the state of a sidebar or modal.
    video: Initialized as an empty array, presumably intended to hold video-related data.
    category: Initialized to "All", possibly used to filter or categorize content.
    searchSuggestion: Initialized as an empty array, likely intended to store search suggestions.

--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -1,5 +1,14 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-
+const appSlice = createSlice({
+    name:"app",
+    initialState:{
+        open:true, 
+        video:[],
+        category:"All",
+        searchSuggestion:[],
+    },
+    
+});
 
 
