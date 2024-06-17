624a89532ec91fe64a346d25bfc233071de9814b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 16:26:59 +0530

    The provided code sets up a React application with routing using react-router-dom. Here's a detailed explanation of the components and functionality:
    Components
    Body: The main container component for the app.
    Navbar: The navigation bar component.
    Watch: Component for watching a specific video.
    Feed: Component that displays the main feed of videos.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Body from "./components/Body";
+import Navbar from "./components/Navbar";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Watch from "./components/Watch";
+import Feed from "./components/Feed";
+
+
+
+
+
+
